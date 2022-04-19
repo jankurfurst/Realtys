3f6a5528c43f5151a528c7617cecba6c356941ae
jankurfurst <85358310+jankurfurst@users.noreply.github.com>
2022-04-19 09:34:59 +0200

    Cleaning

--- Realtys/Views/DetailPage.xaml.cs
+++ Realtys/Views/DetailPage.xaml.cs
@@ -16,30 +16,15 @@ public partial class DetailPage : ContentPage
 
 		var viewModel = (DetailViewModel)BindingContext;
 
-		if (viewModel.mortgage == null || viewModel.realEstate.MortgageUsage == false)
-        {
-			mortgageDetail.IsVisible = false;
-			mortgageCheckBox.IsVisible = false;
-        }
-        else
-        {
-			mortgageDetail.IsVisible = false;
-			mortgageCheckBox.IsVisible = true;
-        }
+		//if (viewModel.mortgage == null || viewModel.realEstate.MortgageUsage == false)
+  //      {
+		//	mortgageDetail.IsVisible = false;
+  //      }
+  //      else
+  //      {
+		//	mortgageDetail.IsVisible = true;
+  //      }
 
 	}
 
-	async void OnCheckBoxCheckedChanged(object sender, CheckedChangedEventArgs e)
-	{
-		await DisplayAlert(nameof(sender), "Realty will be shown:" + e.Value, "OK");
-		realtyDetail.IsVisible = e.Value;
-		
-	}
-
-	async void OnCheckBoxCheckedChangedSecond(object sender, CheckedChangedEventArgs e)
-	{
-		await DisplayAlert("Mortgage will be shown", "Value:" + e.Value, "OK");
-		mortgageDetail.IsVisible = e.Value;
-		
-	}
 }
