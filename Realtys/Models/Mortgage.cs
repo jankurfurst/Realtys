63dcc96b3522fcea1d07aaff895c61076798fd63
jankurfurst <85358310+jankurfurst@users.noreply.github.com>
2022-04-28 22:15:54 +0200

    Added code description

--- Realtys/Models/Mortgage.cs
+++ Realtys/Models/Mortgage.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Realtys.Models
 {
+    /// <summary>
+    /// Model hypotečního úvěru.
+    /// </summary>
     [Table(nameof(Mortgage))]
     public class Mortgage
     {
@@ -13,8 +16,15 @@ namespace Realtys.Models
         [NotMapped]
         private double? interest;
 
+        /// <summary>
+        /// ID úvěru.
+        /// </summary>
         [Key]
         public int ID { get; set; }
+
+        /// <summary>
+        /// Roční úroková sazba úvěru.
+        /// </summary>
         [Required]
         public string Interest
         {
@@ -26,6 +36,9 @@ namespace Realtys.Models
             }
         }
 
+        /// <summary>
+        /// Podíl z ceny nemovitsti, pokrytý úvěrem
+        /// </summary>
         [Required]
         public string Share
         {
@@ -37,8 +50,15 @@ namespace Realtys.Models
             }
         }
 
+        /// <summary>
+        /// Počáteční dluh.
+        /// </summary>
         [Required]
         public double InitialDebt { get; set; }
+        
+        /// <summary>
+        /// Počet let splatnosti úvěru.
+        /// </summary>
         [Required]
         public string ForYears
         {
@@ -49,8 +69,16 @@ namespace Realtys.Models
                 else forYears = null;
             }
         }
+
+        /// <summary>
+        /// Splátka úvěru.
+        /// </summary>
         [Required]
         public double Payment { get; set; }
+
+        /// <summary>
+        /// ID nemovitosti, ke které je úvěr vázán.
+        /// </summary>
         [ForeignKey(nameof(RealEstate))]
         public int RealtyID { get; set; }
 
